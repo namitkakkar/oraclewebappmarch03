26bb519d8c694be91a293e77f0fba24cc58628a0
namitkakkar <namit.kakkar@oracle.com>
2020-03-06 06:36:05 +0000

    add image

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,17 @@
 if  [  "$color"  ==  "black"  ]
 then
         cp  /opt/namit.html   /var/www/html/index.html
+	cp  /opt/docker.png   /var/www/html/
         httpd  -DFOREGROUND
 elif  [   "$color"  ==  "red"    ]
 then
         cp   /opt/red.html   /var/www/html/index.html
+	cp  /opt/docker.png   /var/www/html/
         httpd  -DFOREGROUND
 elif  [   "$color"  ==   "green"     ]
 then
         cp   /opt/green.html   /var/www/html/index.html
+	cp  /opt/docker.png   /var/www/html/
         httpd  -DFOREGROUND
 else
         echo  "Sorry no color found  "   >/var/www/html/index.html
